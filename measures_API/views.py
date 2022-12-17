c2f10cf0b3bf5706a37b2e3ccecb9a2bfa2d53cd
Luis Ovidio Gonzalez Alzate <logonzalez@enertotalesp.com>
2022-12-17 15:14:02 -0500

    creacion vistas para generar archivo csv

--- measures_API/views.py
+++ measures_API/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+import pandas as pd
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Measure
+
+
+def index(request):
+    template = 'index.html'
+    return render(request, template)
+
+
+def download_csv_file(request):
+    qs = Measure.objects.all().values('device_id', 'measure_value', 'measure_unit', 'measure_date')
+
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="measures.csv"'
+
+    df = pd.DataFrame.from_records(qs)
+
+    df.to_csv(response)
+
+    return response
+
